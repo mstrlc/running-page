cdd9f5feb6ee590c7dc8f66d30c8f24af3f0be8f
Matyáš Strelec <matyasstrelec@gmail.com>
2024-06-01 17:59:56 +0200

    metadata fix

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -3,6 +3,10 @@ interface ISiteMetadataResult {
   siteUrl: string;
   description: string;
   logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
 }
 
 const data: ISiteMetadataResult = {
@@ -10,6 +14,16 @@ const data: ISiteMetadataResult = {
   siteUrl: 'https://run.mstrlc.eu',
   logo: 'https://cdsassets.apple.com/live/7WUAS350/images/nav-icons/content-nav-watch-workout-running-icon.png',
   description: 'Personal site and blog',
+  navLinks: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/mstrlc',
+    },
+    {
+      name: 'About',
+      url: 'https://github.com/mstrlc/running_page/blob/master/README-CN.md',
+    },
+  ],
 };
 
 export default data;
